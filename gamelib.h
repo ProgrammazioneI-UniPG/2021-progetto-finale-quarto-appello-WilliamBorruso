cb9207682abe10313c7d91b0a1860494b5955013
WilliamBorruso <33416412+WilliamBorruso@users.noreply.github.com>
2021-04-11 12:26:01 +0200

    Update gamelib.h

--- gamelib.h
+++ gamelib.h
@@ -1,5 +1,28 @@
-// Dichiarazione delle funzioni da chiamare in main.c
-// Definizione dei tipi necessari alla libreria
+#include <stdio.h>
+#include <stdlib.h>
 
+/* Definisco tutti gli enum */
+enum Stato_giocatore {astronauta, impostore, assassinato, defenestrato};
+enum Nome_giocatore {nero, rosso, verde, giallo, blu, magenta, ciano, bianco, arancione, viola};
+enum Tipo_stanza {vuota, quest_semplice, quest_complicata, botola};
 
+/* Definisco tutte le struct */
+struct Giocatore {
+  struct Stanza *posizione;
+  enum Stato_giocatore stato;
+  enum Nome_giocatore nome;
+};
 
+struct Stanza {
+  struct Stanza *avanti;
+  struct Stanza *sinistra;
+  struct Stanza *destra;
+  struct Stanza *stanza_precedente;
+  enum Tipo_stanza tipo;
+  int emergenza_chiamata;
+};
+
+/* Definisco le funzioni */
+void imposta_gioco();
+void gioca();
+void termina_gioco();
